2e7e974826a2db13a610140af5f56d379de6aa1e
mike <60325499+leguim-repo@users.noreply.github.com>
2020-09-14 14:54:34 +0200

    Commit con la estructura general del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Pom Hotel
 Sistema de reservas del Pom Hotel
+
+
+## Acordaros de hacer las modificaciones a traves de git flow:
+
+Uso simple de git flow
+
+```code
+git flow init -d # Solo se debe hacer la primera vez para inicializar el git flow
+git flow feature start <nombredelafeature>
+Ahora modificamos/creamos lo necesario para la feature
+git add .
+git commit
+git flow feature finish
+git push
+````
---
+++ src/main/java/com/pomhotel/booking/ui/controllers/HomeController.java
@@ -0,0 +1,56 @@
+package com.pomhotel.booking.ui.controllers;
+
+import org.apache.catalina.connector.CoyoteInputStream;
+import org.dom4j.rule.Mode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.xml.transform.stream.StreamSource;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.Date;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    public HomeController() { }
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("projectname", "Pom Hotel & SPA");
+
+        model.addAttribute("mode", "Test");
+
+        return "index";
+    }
+
+    @GetMapping("/petar")
+    public ModelAndView forzar500(Model model) {
+        // metodo para forzar un error 500 y ver la pagina de error
+        Integer a;
+        a = 1/0;
+        return new ModelAndView();
+    }
+
+    /*
+    https://www.generateit.net/javascript-decompressor/
+    https://onlineasciitools.com/convert-decimal-to-ascii
+    https://beautifier.io/
+    */
+    @PostMapping("/reserve")
+    public String acceptData(@RequestBody String payloadBody, @RequestHeader HttpHeaders headers)  {
+        // Con este metodo podemos ver que paramentros enviamos con el post desde el form
+        System.out.println("\nParametros recibidos: "+payloadBody+"\n");
+        return "index";
+    }
+
+
+}
