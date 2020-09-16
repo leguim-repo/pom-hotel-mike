e691301ea9dcd5309d22b7ec566f4c594966da92
mike <60325499+leguim-repo@users.noreply.github.com>
2020-09-16 08:56:25 +0200

    Creado e implementado LoginRepository

--- src/main/java/com/pomhotel/booking/application/repositories/LoginRepository.java
+++ src/main/java/com/pomhotel/booking/application/repositories/LoginRepository.java
@@ -2,7 +2,18 @@ package com.pomhotel.booking.application.repositories;
 
 import com.pomhotel.booking.application.domain.entities.ClientsEntity;
 import com.pomhotel.booking.application.domain.entities.LoginsEntity;
+import org.apache.juli.logging.Log;
+
+import java.util.List;
+import java.util.Set;
 
 public interface LoginRepository {
+    //TODO refactor findByUsername -> findByUserName
+    LoginsEntity authentification (LoginsEntity entity);
+    LoginsEntity findById(long id);
     LoginsEntity findByUsername (String username);
+    List<LoginsEntity> findAll();
+    void createNewLogin(LoginsEntity entity);
+    void updateLogin(LoginsEntity entity);
+    void deleteLogin(long id);
 }
--- src/main/java/com/pomhotel/booking/application/repositories/LoginRepositoryImplementation.java
+++ src/main/java/com/pomhotel/booking/application/repositories/LoginRepositoryImplementation.java
@@ -3,12 +3,17 @@ package com.pomhotel.booking.application.repositories;
 import com.pomhotel.booking.application.domain.entities.LoginsEntity;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
 
 @Repository
 public class LoginRepositoryImplementation implements LoginRepository{
     private static SessionFactory dbConnection;
+
     public LoginRepositoryImplementation() {
             // Configuracion de Full Hibernate
             try {
@@ -24,6 +29,26 @@ public class LoginRepositoryImplementation implements LoginRepository{
     }
 
     @Override
+    public LoginsEntity authentification(LoginsEntity entity) {
+        // TODO pendiente de ver la implementacion que necesitamos
+        return null;
+    }
+
+    @Override
+    public LoginsEntity findById(long id) {
+        LoginsEntity logins = null;
+        Session session = this.dbConnection.openSession();
+        try {
+            logins = session.get(LoginsEntity.class, id);
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return logins;
+    }
+
+    @Override
     public LoginsEntity findByUsername(String username) {
         LoginsEntity login = null;
         try (Session session = dbConnection.openSession()) {
@@ -32,6 +57,72 @@ public class LoginRepositoryImplementation implements LoginRepository{
             ex.printStackTrace();
         }
         return login;
+    }
+
+    @Override
+    public List<LoginsEntity> findAll() {
+        List<LoginsEntity> allLogins = null;
+        Session session = this.dbConnection.openSession();
+        try {
+            CriteriaQuery<LoginsEntity> cq = session.getCriteriaBuilder().createQuery(LoginsEntity.class);
+            cq.select(cq.from(LoginsEntity.class));
+            allLogins = session.createQuery(cq).getResultList();
+        }catch (Throwable ex) {
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return allLogins;
+    }
+
+    @Override
+    public void createNewLogin(LoginsEntity entity) {
+        Session session = this.dbConnection.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        }catch (Throwable ex) {
+            if (transaction!=null) transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+    }
+
+    @Override
+    public void updateLogin(LoginsEntity entity) {
+        Session session = this.dbConnection.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.update(entity);
+            transaction.commit();
+        }catch (Throwable ex) {
+            if (transaction!=null) transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public void deleteLogin(long id) {
+        Session session = this.dbConnection.openSession();
+        Transaction transaction = null;
+        try {
+            LoginsEntity tareaABorrar = session.load(LoginsEntity.class, id);
+            transaction = session.beginTransaction();
+            session.delete(tareaABorrar);
+            transaction.commit();
+        }catch (Throwable ex) {
+            if (transaction!=null) transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 
 
