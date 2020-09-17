38d07adf4c3294450a432a8646ace9f0fbe798c3
Pablo <pablo@pom.es>
2020-09-17 16:10:16 +0200

    add Cl_serv_interface

---
+++ src/main/java/com/pomhotel/booking/application/services/ClientService.java
@@ -0,0 +1,12 @@
+package com.pomhotel.booking.application.services;
+
+import com.pomhotel.booking.application.models.ClientsModel;
+import java.util.List;
+
+public interface ClientService {
+    ClientsModel findById(long id);
+    List<ClientsModel> findAll();
+    void saveOrUpdate(ClientsModel model);
+    void deleteById(long id);
+    void delete(ClientsModel model);
+}
