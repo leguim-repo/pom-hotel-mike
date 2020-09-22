d75d42f0323b74d011c9d89b39b641c169bcaf3f
mike <60325499+leguim-repo@users.noreply.github.com>
2020-09-22 20:18:25 +0200

    Spring Security Added

---
+++ src/main/java/com/pomhotel/booking/ui/configuration/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,19 @@
+package com.pomhotel.booking.ui.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        System.out.println("onAuthenticationFailure: "+e.getMessage());
+        //e.printStackTrace();
+    }
+}
--- src/main/java/com/pomhotel/booking/ui/controllers/HomeController.java
+++ src/main/java/com/pomhotel/booking/ui/controllers/HomeController.java
@@ -1,6 +1,5 @@
 package com.pomhotel.booking.ui.controllers;
 
-import com.pomhotel.booking.application.models.LoginsModel;
 import com.pomhotel.booking.application.models.RoomsModel;
 import com.pomhotel.booking.application.services.LoginService;
 import com.pomhotel.booking.application.services.RoomsService;
@@ -34,7 +33,7 @@ public class HomeController {
     @GetMapping("/home")
     public String home(Model model) {
         model.addAttribute("strNav", "Find Emotions");
-        model.addAttribute("imgNav", "chica_piscina.jpg");
+        model.addAttribute("imgNav", "chica_jacuzzi.jpg");
         return "home";
     }
 
@@ -49,17 +48,18 @@ public class HomeController {
         return "listrooms";
     }
 
+
     //OTHERS *MIKEEE*
-    @GetMapping("/registrationform")
-    public String registration(Model model) {
+    //@GetMapping("/registrationform")
+    //public String registration(Model model) {
         //model.addAttribute("userForm", new User());
 
-        return "registrationform";
-    }
+    //    return "registrationform";
+    //}
 
-    @PostMapping("/registrationform")
+    //@PostMapping("/registrationform")
     //public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
-    public String registration() {
+    //public String registration() {
         /*
         userValidator.validate(userForm, bindingResult);
         if (bindingResult.hasErrors()) {
@@ -69,10 +69,10 @@ public class HomeController {
         securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
          */
 
-        return "redirect:/";
-    }
+    //    return "redirect:/";
+    //}
 
-
+    /*
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
         if (error != null)
@@ -83,21 +83,21 @@ public class HomeController {
 
         return "login";
     }
-
+    */
 
     // TODO petarme authentificate login/resitration is the way
     // login entry point
-    @PostMapping("/authentificate")
-    public ModelAndView loginView(Model model) {
+    //@PostMapping("/authentificate")
+    //public ModelAndView loginView(Model model) {
         // recogemos las credenciales de cliente y llamamos al servicio para que las compruebe
-        String logincorrect = "logincorrect";
-        String loginfail = "loginfail";
-        String view = "";
+    //    String logincorrect = "logincorrect";
+    //    String loginfail = "loginfail";
+    //    String view = "";
         /*
          si el servicio valida la credenciales que...que hacemos...como decimos al cliente que esta logeado correctamente?
          si las credenciales no son validas le tiramos un pop up de que no son validas
          */
-
+    /*
         if (true) {
             view = logincorrect;
         }
@@ -106,18 +106,18 @@ public class HomeController {
         }
         return new ModelAndView(view);
     }
-
+    */
 
 
 
     // prebooking entry point
-    @PostMapping("/prebooking")
-    public ModelAndView booknow(Model model) {
+    //@PostMapping("/prebooking")
+    //public ModelAndView booknow(Model model) {
         /*
          si el cliente esta conectado pasa la vista de checkavail
          si el cliente no esta conectado pasamos a al vista de login
         */
-
+/*
         String loginview = "login";
         String checkavail = "checkavail";
         String view = "";
@@ -129,12 +129,14 @@ public class HomeController {
         }
         return new ModelAndView(view);
     }
-
+*/
+    /*
     @PostMapping("/booking")
     public ModelAndView checkdates(Model model){
         // comprobar login de cliente por seguridad
         return new ModelAndView("booked");
     }
+    */
 
     // for test purposes
     @PostMapping("/mike")
