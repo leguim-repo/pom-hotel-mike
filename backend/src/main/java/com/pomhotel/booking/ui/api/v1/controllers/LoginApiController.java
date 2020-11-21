262865738307cbd85ee4f62e65b566f6acb11cab
mike <60325499+leguim-repo@users.noreply.github.com>
2020-11-21 20:43:40 +0100

    TODO JWT

--- backend/src/main/java/com/pomhotel/booking/ui/api/v1/controllers/LoginApiController.java
+++ backend/src/main/java/com/pomhotel/booking/ui/api/v1/controllers/LoginApiController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
+//TODO https://github.com/bezkoder/spring-boot-spring-security-jwt-authentication
 //@CrossOrigin(origins = "http://pom-hotel.code:3000", maxAge = 3600, allowCredentials = "true")
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RestController
