f6c8cc70251ced458800d0e8958153cd8a4f9858
mike <60325499+leguim-repo@users.noreply.github.com>
2021-01-18 19:35:04 +0100

    Increase delay in order to create/populate database. For any unkwown reason today I need more time

--- docker/DockerMySQL/01_build.sh
+++ docker/DockerMySQL/01_build.sh
@@ -6,12 +6,12 @@ function waitinXsec() {
 source 10_CreatePersitenceVolume.sh
 source 20_CreateImage.sh
 source 98_stopPom-MySQL-Container.sh
-waitinXsec 5
+waitinXsec 10
 source 99_startPom-MySQL-Container.sh
-waitinXsec 5
+waitinXsec 10
 source 50_CreateDB.sh
 source 51_PopulateDB.sh
 docker ps
 source 98_stopPom-MySQL-Container.sh
-waitinXsec 5
+waitinXsec 10
 docker ps
